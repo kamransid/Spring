41a42e2258abe7466f2b5b02ad0fd7253013c402
Siddique <mdkamran.siddique@cgi.com>
2018-07-13 02:13:41 +0530

    My First Commit Form CMD

---
+++ src/com/kam/Testing.java
@@ -0,0 +1,33 @@
+package com.kam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Map map= new HashMap();
+		
+		map.put(1, "3");
+		map.put("kallu", "bjai");
+		
+		List list = new ArrayList();
+		
+		list.add("Bhai");
+		list.add(1);
+		
+
+	}
+	 abstract void populate();
+	 
+	 void populate1(){
+		//return x;
+		 
+		 List list;
+		 }
+
+}
